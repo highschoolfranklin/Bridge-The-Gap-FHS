ba922e923a810fec8689113c95bc0bb2acafd45f
highschoolfranklin <highschoolfranklin9@gmail.com>
2026-04-05 02:59:52 -0700

    Update EmailJS service ID and public key

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const SUPABASE_URL = "https://ovxmcrqqsadyrypsizfe.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92eG1jcnFxc2FkeXJ5cHNpemZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzNzY1NDUsImV4cCI6MjA5MDk1MjU0NX0.xGNNI1NyxyEdsGuxMx9gFtTRjPnRYegutd0PeXHSgcI";
 
 // EmailJS Configuration
-const EMAILJS_SERVICE_ID = "GRS6z9jufvltQJ6H0";
-const EMAILJS_PUBLIC_KEY = "GRS6z9jufvltQJ6H0";
+const EMAILJS_SERVICE_ID = "service_a28vktd";
 const EMAILJS_TEMPLATE_LETTER = "template_szo5gvp";
 const EMAILJS_TEMPLATE_REMINDER = "template_z2sah5c";
 const SENDER_EMAIL = "highschoolfranklin9@gmail.com";
